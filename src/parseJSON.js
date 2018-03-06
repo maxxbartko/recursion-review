dfd5ffd7df64c8746c3d9926407a9e81f405c9c2
maxxbartko <maxx.bartko@gmail.com>
2018-03-05 18:25:10 -0800

    Began parseJSON, got hung up on number conditionals and how to split objects

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,36 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
-};
+  // if (parseFloat(json) !== NaN) {
+  //    return parseFloat(json);
+  //  } else 
+  if (json[0] === '[') { //array
+    var arr = [];
+    var testString = json.slice(1, json.length-1);
+    testString.split(',').forEach(function(v, i) {
+      arr.push(parseJSON(v));
+    });
+    return arr;
+  } else if (json[0] === '{') { //object
+    var obj = {};
+    var testString = json.slice(1, json.length-1);
+    //iteration over split by comma
+    ///split by colon and assign key/value pairs
+    ///add to obj
+    testString.split(',').forEach(function(v, i) {
+
+    });
+    return obj;
+  } else if(json === 'null'){ return null;
+  } else if(parseFloat(json) !== NaN){
+    return parseFloat(json);
+  } else if (json === 'true' || 'false') {
+    return json === "true" ? true : false;
+  } else { //string
+    return json; //json.slice(1, json.length - 1);
+  }
+}
+
+//input string
+//output text in string in type implied by string
+
